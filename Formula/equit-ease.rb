fd31d8c6bf6041f11c69c0710de2ca1c195f1661
dan murphy <danielmurph8@gmail.com>
2021-01-18 12:39:48 -0500

    add installation

--- Formula/equit-ease.rb
+++ Formula/equit-ease.rb
@@ -151,4 +151,8 @@ class EquitEase < Formula
     url "https://files.pythonhosted.org/packages/89/38/459b727c381504f361832b9e5ace19966de1a235d73cdbdea91c771a1155/wcwidth-0.2.5.tar.gz"
     sha256 "c4d647b99872929fdb7bdcaa4fbe7f01413ed3d98077df798530e5b04f116c83"
   end
+
+  def install
+    virtualenv_install_with_resources
+  end
 end
